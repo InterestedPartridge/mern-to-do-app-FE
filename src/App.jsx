e80f9e27482832f9346265756d64053100f11f66
012_Aryan Bichpuria <bichpuriar471@gmail.com>
2023-07-27 01:25:39 +0530

    error in getting profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import { Toaster } from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import { useContext, useEffect } from "react";
 import axios from "axios";
 import { Context, server } from "./main";
