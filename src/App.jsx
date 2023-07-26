422fdeb6a8e61f4baedad87844c4d070540ac1e9
012_Aryan Bichpuria <bichpuriar471@gmail.com>
2023-07-27 01:57:05 +0530

    toaster error

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { toast } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import { useContext, useEffect } from "react";
 import axios from "axios";
 import { Context, server } from "./main";
